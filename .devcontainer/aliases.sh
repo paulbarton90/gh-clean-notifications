561e2cc8fa31f0b1c2c0c500aaa6f1578a00a6a5
Paul Barton <28630076+paulbarton90@users.noreply.github.com>
2023-12-26 12:28:28 +0000

    Add autoSetupRemote

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Setup git aliases
 git config alias.d 'diff'
 git config alias.ds 'diff --staged'
 git config alias.st 'status'
@@ -7,3 +8,6 @@ git config alias.co 'checkout'
 git config alias.br 'branch'
 git config alias.s 'status'
 git config alias.conf 'config --list --local'
+
+# Setup misc configuration
+git config --add --bool push.autoSetupRemote true
