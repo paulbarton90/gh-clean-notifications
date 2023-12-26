13d86afcb06036b44619a797086b0545bb9e60b8
Paul Barton <28630076+paulbarton90@users.noreply.github.com>
2023-12-26 12:25:24 +0000

    Update basic devcontainer test

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config alias.d 'diff'
+git config alias.ds 'diff --staged'
+git config alias.st 'status'
+git config alias.co 'checkout'
+git config alias.br 'branch'
+git config alias.s 'status'
+git config alias.conf 'config --list --local'
